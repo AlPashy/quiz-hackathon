3557c633d0cd99eb79468c0d320e6bfced7f237f
Deniz Heinzelmann <deniz.heinzelmann@atruvia.de>
2024-01-25 14:39:48 +0100

    Higher Lower Game

--- src/js/project.js
+++ src/js/project.js
@@ -37,58 +37,42 @@ var questions = [
     {
         question: "Welche bekannten Kunden oder Partner hat die Atruvia AG?",
         options: ["Tesla", "Microsoft", "Volksbanken", "Alle der oben genannten"],
-        correctAnswerIndex: 2 
+        correctAnswerIndex: 2
     }
 ];
 
-var questionsSecond = [ {
-        question: "Verwaltet die Atruvia mehr, oder weniger als 70.000.000 Konten?",
-        options: ["Mehr", "Weniger"],
-        correctAnswerIndex: 0   
-    },
-    {
-        question: "Verwaltet die Atruvia mehr, oder weniger als 70.000.000 Konten?",
-        options: ["Mehr", "Weniger"],
-        correctAnswerIndex: 0   
-    },
-    {
-        question: "Verwaltet die Atruvia mehr, oder weniger als 70.000.000 Konten?",
-        options: ["Mehr", "Weniger"],
-        correctAnswerIndex: 0   
-    },
-    {
-        question: "Verwaltet die Atruvia mehr, oder weniger als 70.000.000 Konten?",
-        options: ["Mehr", "Weniger"],
-        correctAnswerIndex: 0   
-    },
-    {
-        question: "Verwaltet die Atruvia mehr, oder weniger als 70.000.000 Konten?",
-        options: ["Mehr", "Weniger"],
-        correctAnswerIndex: 0   
-    },
-    {
-        question: "Verwaltet die Atruvia mehr, oder weniger als 70.000.000 Konten?",
-        options: ["Mehr", "Weniger"],
-        correctAnswerIndex: 0   
-    },
-    {
-        question: "Verwaltet die Atruvia mehr, oder weniger als 70.000.000 Konten?",
-        options: ["Mehr", "Weniger"],
-        correctAnswerIndex: 0   
-    },
-    {
-        question: "Verwaltet die Atruvia mehr, oder weniger als 70.000.000 Konten?",
-        options: ["Mehr", "Weniger"],
-        correctAnswerIndex: 0   
-    }
-];
 var currentQuestionIndex = 0;
+var correctAnswersFr = 0;
+var timer;
+
 document.addEventListener('DOMContentLoaded', function () {
-        // Initial setup
     updateQuestion();
+    startTimer();
 });
 
-function updateQuestion(){
+function startTimer() {
+    var timerDisplay = document.getElementById('timer');
+    var timeLeft = 100;
+
+    function updateTimerDisplay() {
+        timerDisplay.textContent = 'Time left: ' + timeLeft + 's';
+    }
+
+    function countdown() {
+        if (timeLeft > 0) {
+            timeLeft--;
+            updateTimerDisplay();
+            timer = setTimeout(countdown, 1000);
+        } else {
+            displayScore();
+        }
+    }
+
+    updateTimerDisplay();
+    timer = setTimeout(countdown, 1000);
+}
+
+function updateQuestion() {
     var questionContainer = document.getElementById('questionContainer');
     var currentQuestion = questions[currentQuestionIndex];
     // Clear previous Q
@@ -100,44 +84,43 @@ function updateQuestion(){
     var answerOptions = document.createElement('ul');
     answerOptions.className = "answer-options";
 
-    currentQuestion.options.forEach(function(option, index){
+    currentQuestion.options.forEach(function (option, index) {
         var button = document.createElement('button');
-        button.className ='button-style';
+        button.className = 'button-style';
         button.dataset.option = 'option' + (index + 1);
         button.textContent = option;
-        button.onclick = function(){
+        button.onclick = function () {
             checkAnswer('option' + (index + 1));
         };
         var li = document.createElement('li');
         li.appendChild(button);
         answerOptions.appendChild(li);
-
     });
-    questionContainer.appendChild(answerOptions);   
+
+    questionContainer.appendChild(answerOptions);
 }
-var correctAnswersFr = 0;
 
 function checkAnswer(selectedOption) {
-        var selectedRadioButton = document.querySelector('input[name="slide"]:checked');
+    var selectedRadioButton = document.querySelector('input[name="slide"]:checked');
     var answerButtons = selectedRadioButton.parentElement.querySelectorAll('.button-style');
     var correctAnswerButton = answerButtons[questions[currentQuestionIndex].correctAnswerIndex];
     var userAnswerButton = selectedRadioButton.parentElement.querySelector('.button-style[data-option="' + selectedOption + '"]');
     answerButtons.forEach(function (button) {
         button.disabled = true;
     });
-    console.log('Selected Option:', selectedOption);
-    console.log('User Answer Button:', userAnswerButton);
-    console.log('Correct Answer Button:', correctAnswerButton);
+
     if (userAnswerButton !== correctAnswerButton) {
-        console.log('Incorrect Answer!');
         userAnswerButton.style.backgroundColor = 'red';
     } else {
-        console.log('Correct Answer!');
         userAnswerButton.style.backgroundColor = 'green';
         correctAnswersFr++;
     }
+
     correctAnswerButton.style.backgroundColor = 'green';
-    console.log(correctAnswersFr)
+
+    setTimeout(function () {
+        playGame();
+    }, 500);
 }
 
 function playGame() {
@@ -146,18 +129,17 @@ function playGame() {
     } else {
         document.querySelector('.container').classList.add('hidden');
         setTimeout(function () {
-            currentQuestionIndex = (currentQuestionIndex + 1) % questions.length;
-            var answerButtons = document.querySelectorAll('.button-style');
-            answerButtons.forEach(function (button) {
-                button.style.backgroundColor = '';
-                button.disabled = false;
-            });
-            if (currentQuestionIndex === 0) {
+            if (currentQuestionIndex === questions.length - 1) {
                 // Display user's score
                 displayScore();
             } else {
+                currentQuestionIndex = (currentQuestionIndex + 1) % questions.length;
+                var answerButtons = document.querySelectorAll('.button-style');
+                answerButtons.forEach(function (button) {
+                    button.style.backgroundColor = '';
+                    button.disabled = false;
+                });
                 updateQuestion();
-               
                 document.querySelector('.container').classList.remove('hidden');
             }
         }, 500);
@@ -169,18 +151,21 @@ function displayScore() {
     var questionContainer = document.getElementById('questionContainer');
     questionContainer.innerHTML = '';
     var scoreHeading = document.createElement('h4');
-    scoreHeading.textContent = 'Your Score: ' + correctAnswersFr + ' out of ' + questions.length;
+    scoreHeading.textContent = 'Your Score: ' + score + ' out of ' + questions.length;
     scoreHeading.style.fontSize = '25px';
 
     questionContainer.appendChild(scoreHeading);
     var message = document.createElement('p');
-    message.style.fontSize ='25px';
-    if (score >= 6) {
-        message.textContent = 'You win! 👑 Get outta here';
+    message.style.fontSize = '25px';
+
+    if (score === questions.length) {
+        message.textContent = 'You win! 👑';
     } else {
         message.textContent = 'You lose! 😂';
     }
+
     questionContainer.appendChild(message);
+
     var retryButton = document.createElement('button');
     retryButton.textContent = 'Retry';
     retryButton.className = 'button-style';
@@ -188,7 +173,10 @@ function displayScore() {
         resetQuiz();
     };
     questionContainer.appendChild(retryButton);
+
+    clearTimeout(timer);
 }
+
 
 function resetQuiz() {
     currentQuestionIndex = 0;
@@ -199,21 +187,14 @@ function resetQuiz() {
         button.disabled = false;
     });
     updateQuestion();
-
+    resetTimer();
 }
 
 function calculateScore() {
-    var correctAnswers = 0;
-    questions.forEach(function (question, index) {
-        var selectedOption = 'option' + (question.correctAnswerIndex + 1);
-        var userAnswerButton = document.querySelector('.button-style[data-option="' + selectedOption + '"]:disabled');
-        
-        if (userAnswerButton) {
-            if (userAnswerButton.style.backgroundColor === 'green') {
-                correctAnswers++;
-            }
-        }
-    });
-    console.log('Correct Answers:', correctAnswers);
-    return correctAnswers;
+    return correctAnswersFr;
+}
+
+function resetTimer() {
+    clearTimeout(timer);
+    startTimer();
 }
